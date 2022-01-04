054cd94435f96d248e08d05b1c87cf3dc2d441d9
Jeffrey Zhen <jlrzhen@uwaterloo.ca>
2022-01-03 23:54:13 -0500

    Added testing folder

--- runTests.sh
+++ runTests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source config
 source repo
+#TODO: Add environment variables for config path
 cd $DIR/testing && git clone $REPO; echo
 cat $DIR/inputs | python3 $DIR/testing/*/*.py 
 rm -rf $DIR/testing/*
+touch $DIR/testing/.gitkeep 

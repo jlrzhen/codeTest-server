ba99089636b8b5cb1eb824ca4d9c517b5f0e6f31
Jeffrey Zhen <jlrzhen@uwaterloo.ca>
2022-01-03 23:53:32 -0500

    Enabled url build trigger

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def submit(user, repo):
     f = open("repo", "w")
     f.write('{remote}/{user}/{repo}'.format(remote=endpoints.remote_repo, user=user, repo=repo))
     f.close()
-    #requests.get(endpoints.trigger_build)
+    requests.get(endpoints.trigger_build)
     time.sleep(5)
     f2 = open("repo", "w")
     f2.write('')
